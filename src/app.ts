bc534ee969d9ad404ed471450b355663b92ae950
Claudia <clau.neme@gmail.com>
2022-04-11 09:59:56 -0300

    Se agrego la configuracion de cloudinary. Se agrego un errorHandler como middleware, y se dejo documentado como usarlo en errorConstructor

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,7 @@ app.set('port', process.env.PORT || 5000);
 //middlewares
 app.use(morgan('dev'));
 app.use(cors());
+//estas configuraciones las vamos a necesitar para recibir desde el front las imagenes
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
