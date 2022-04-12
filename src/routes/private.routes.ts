49f001d13eb5085fae47ad7ada6cfc608caa38b2
Daniela Bedoya Poveda <danielapoveda75@gmail.com>
2022-04-11 21:51:24 -0500

    create and update product controller

--- src/routes/private.routes.ts
+++ src/routes/private.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+const productController = require("../controllers/products/product.controllers");
 
 const router = Router();
 
-
+router.put('/:id', productController.update);
+router.post('/', productController.add);
 
 module.exports = router;
