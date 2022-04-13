bdc615fe4ea593a5a303534e1248cc01c7e872bb
GenaBercini <bercinigena@gmail.com>
2022-04-13 12:34:32 -0300

    Add admin routes

---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+const categoriesController = require("../controllers/categories/categories.controllers");
+
+const router = Router();
+router.post('/category', categoriesController.add)
+
+module.exports = router;
