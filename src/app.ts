27c621587c5c4a64dcc000db5266f92aa5c0feb0
GenaBercini <bercinigena@gmail.com>
2022-04-13 12:39:26 -0300

    Add admin routes

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 //use routes
 app.use('/api/private', require('./routes/private.routes'));
 app.use('/api/public', require('./routes/public.routes'));
-app.use('/api/admin', require('./routes/admin.routes'));
+// app.use('/api/admin', require('./routes/admin.routes'));
 
 //ErrorHandler debe estar despues de todas las rutas
 app.use(errorHandler);
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 const categoriesController = require("../controllers/categories/categories.controllers");
 
 const router = Router();
-router.post('/category', categoriesController.add)
+// router.post('/category', categoriesController.add)
 
 module.exports = router;
